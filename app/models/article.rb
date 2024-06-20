e2b7adb9ed834a17aa966f5e7b36b5e29873d090
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-06-20 15:50:23 +0300

    Added Action Text

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  has_rich_text :content
+
   validates :title, length: { minimum: 5, maximum: 100 }
-  validates :body, length: { minimum: 5 }
+  validates :content, length: { minimum: 5 }
 end
---
+++ db/migrate/20240620123108_move_body_to_arction_text.rb
@@ -0,0 +1,9 @@
+class MoveBodyToArctionText < ActiveRecord::Migration[7.1]
+  def change
+    Article.all.find_each do |article|
+      article.update(content: article.body)
+    end
+
+    remove_column :articles, :body
+  end
+end
