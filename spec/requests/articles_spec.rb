dd5b5042605fb91c44a5fb21984622a88651183b
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-06-20 10:38:21 +0300

    adedd tests for create updare create Article

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,7 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe "Articles", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "ArticlesController", type: :request do
+  describe "POST /articles" do
+    it "create a new article" do
+      article_params = { article: { title: "Test Article", body: "Test Body" } }
+
+      expect{
+        post"/articles", params: article_params
+      }.to change(Article, :count).by(1)
+
+      expect(response).to redirect_to(article_path(Article.last))
+    end
+  end
+
+  describe "PATCH /articles/:id" do
+    let!(:article) { Article.create(title: "Initial Title", body: "Initial Body") }
+
+    it "updates an existing article" do
+      updated_title = "Updated Title"
+      updated_body = "Updated Body"
+
+      patch "/articles/#{article.id}", params: { article: { title: updated_title, body: updated_body } }
+
+      article.reload
+
+      expect(article.title).to eq(updated_title)
+      expect(article.body).to eq(updated_body)
+    end
+  end
+  
+  describe "DELETE /articles/:id" do
+    let!(:article) { Article.create(title: "Initial Title", body: "Initial Body") }
+    
+    it "deletes an existing article" do
+      expect {
+        delete "/articles/#{article.id}"
+      }.to change(Article, :count).by(-1)
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -91,4 +91,9 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.formatter = :documentation
+
+  config.after(:suite) do
+    puts "\nВсе тесты прошли успешно! 🎉"
+  end
 end
