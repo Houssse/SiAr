8d949d32a6b60834d96ca9122d41bb71e201c725
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-06-21 12:24:27 +0300

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,30 @@
 require 'faker'
 
+# Создаем случайное количество пользователей от 5 до 10
+num_users = rand(5..10)
+
+# Массив для хранения созданных пользователей
+users = []
+
+num_users.times do
+  # Создаем пользователя
+  user = User.create!(
+    name: Faker::Name.unique.name,
+    email: Faker::Internet.unique.email,
+    password: 'password'
+  )
+  
+  users << user 
+end
+
 10.times do
+  # Выбираем случайного пользователя из массива users
+  random_user = users.sample
+  
   Article.create!(
     title: Faker::Book.title,
-    content: Faker::Lorem.paragraph(sentence_count: 10)
+    content: Faker::Lorem.paragraph(sentence_count: 50),
+    user: random_user  # Привязываем статью к выбранному пользователю
   )
 end
 
