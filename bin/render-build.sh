c33f895c2c07d94801cba434b3c09e264df8d9a2
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-06-25 15:43:45 +0300

    Fix Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,15 +8,11 @@ yarn install
 
 # Предварительная компиляция ассетов
 echo "Precompiling assets..."
-bundle exec rake assets:precompile
-
-# Очистка предыдущих ассетов (если необходимо)
-echo "Cleaning assets..."
-bundle exec rake assets:clean
+bundle exec rake assets:precompile RAILS_ENV=production
 
 # Удаление и создание базы данных
 echo "Dropping and recreating the database..."
-bundle exec rake db:drop RAILS_ENV=production
+bundle exec rake db:drop RAILS_ENV=production || true  # Используем || true, чтобы игнорировать ошибку, если база данных не существует
 bundle exec rake db:create RAILS_ENV=production
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec rake db:seed RAILS_ENV=production
