7cc1b53049201607c625a6af7df2faa31e9eed0b
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-09-12 11:45:43 +0300

    add test comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  let(:user) { create(:user) }
+  let(:article) { create(:article) }
+  let(:comment) { build(:comment, user: user, article: article) }
+
+  it 'is created successfully if the user is authenticated' do
+    expect(comment).to be_valid
+  end
+
+  it 'is not created if the user is not authenticated' do
+    comment.user = nil
+    expect(comment).not_to be_valid
+  end
 end
--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,6 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
-  describe "GET /index" do
+  let(:user) { create(:user) }
+  let(:admin) { create(:user, email: 'admin@example.com', admin: true) }
+  let(:other_user) { create(:user) }
+  let(:article) { create(:article, user: user) }
+  let(:comment) { create(:comment, user: user, article: article) }
+
+  describe 'DELETE /articles/:article_id/comments/:id' do
+    context 'authenticated user' do
+      before { sign_in user }
+
+      it 'can delete their own comment' do
+        delete article_comment_path(article, comment)
+        expect(response).to redirect_to(article_path(article))
+        expect(Comment.exists?(comment.id)).to be_falsey
+      end
+    end
+
+    context 'admin user' do
+      before { sign_in admin }
+
+      it 'can delete any comment' do
+        delete article_comment_path(article, comment)
+        expect(response).to redirect_to(article_path(article))
+        expect(Comment.exists?(comment.id)).to be_falsey
+      end
+    end
+  end
+
+  describe 'POST /articles/:article_id/comments' do
+    context 'authenticated user' do
+      before { sign_in user }
+
+      it 'can create a comment' do
+        expect {
+          post article_comments_path(article), params: { comment: { body: 'Test comment' } }
+        }.to change(Comment, :count).by(1)
+
+        expect(response).to redirect_to(article_path(article))
+      end
+    end
   end
 end
